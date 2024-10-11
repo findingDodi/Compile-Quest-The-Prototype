eb79edb65a82c846bc95ef14156e7c55ea451544
findingDodi <dorina.diem@gmail.com>
2024-10-11 20:16:10 +0200

    change szene when player moves out right side

--- CompileQuest/src/Utils.js
+++ CompileQuest/src/Utils.js
@@ -2,5 +2,6 @@
 export class Utils {
     static SCREEN_CENTER_X = 1920/2;
     static SCREEN_CENTER_Y = 1080/2;
+    static SCREEN_END_X = 1920;
 }
 
--- CompileQuest/src/gameobjects/Player.js
+++ CompileQuest/src/gameobjects/Player.js
@@ -17,7 +17,7 @@ export class Player extends Physics.Arcade.Image {
     move(direction) {
         if (direction === "left" && this.x - 10 > 0) {
             this.x -= 5;
-        } else if (direction === "right" && this.x + 10 < 1920) {
+        } else if (direction === "right" && this.x < 2000) {
             this.x += 5;
         }
     }
@@ -25,7 +25,9 @@ export class Player extends Physics.Arcade.Image {
     update() {
         // wiggle
         //this.x += Math.sin(this.scene.time.now / 200) * 0.10;
-        console.log(this.x, this.y)
+        if (this.x > Utils.SCREEN_END_X) {
+            this.scene.scene.start('GameOver');
+        }
     }
 
 }
